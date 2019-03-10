73f40ecdc713ec0aa5381f93726fbd80a14e5086
xatier <xatierlike@gmail.com>
2019-03-09 22:05:22 -0800

    Add who-to-unfollow script and utils.py

---
+++ utils.py
@@ -0,0 +1,67 @@
+import json
+import logging
+import typing
+
+import twitter
+
+import api_keys
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(logging.StreamHandler())
+
+
+def dump(j) -> None:
+    if isinstance(j, twitter.models.TwitterModel):
+        j = j.AsDict()
+    LOGGER.info(json.dumps(j, sort_keys=True, indent=2))
+
+
+def verify(api: twitter.api.Api) -> None:
+    dump(api.VerifyCredentials())
+
+
+def get_rate_limit(
+    api: twitter.api.Api
+) -> twitter.ratelimit.EndpointRateLimit:
+    return api.CheckRateLimit('https://api.twitter.com/1.1/friends/list.json')
+
+
+def login() -> twitter.api.Api:
+    return twitter.Api(
+        consumer_key=api_keys.consumer_key,
+        consumer_secret=api_keys.consumer_secret,
+        access_token_key=api_keys.access_token_key,
+        access_token_secret=api_keys.access_token_secret,
+        sleep_on_rate_limit=True
+    )
+
+
+def get_friends(
+    api: twitter.api.Api, screen_name: str
+) -> typing.List[twitter.models.User]:
+    friends = []
+
+    try:
+        friends = api.GetFriends(screen_name=screen_name)
+    except twitter.error.TwitterError as e:
+        LOGGER.warning('Twitter error %s, skipping %s', e, screen_name)
+    except Exception as e:
+        LOGGER.error('Unknown error %s, skipping %s', e, screen_name)
+
+    return friends
+
+
+def get_followers(
+    api: twitter.api.Api, screen_name: str
+) -> typing.List[twitter.models.User]:
+    followers = []
+
+    try:
+        followers = api.GetFollowers(screen_name=screen_name)
+    except twitter.error.TwitterError as e:
+        LOGGER.warning('Twitter error %s, skipping %s', e, screen_name)
+    except Exception as e:
+        LOGGER.error('Unknown error %s, skipping %s', e, screen_name)
+
+    return followers
---
+++ wtuf.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import logging
+from typing import List, Tuple
+
+import twitter
+
+import utils
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+LOGGER.addHandler(logging.StreamHandler())
+
+
+def go(api: twitter.api.Api, start_user: str) -> Tuple[
+    List[twitter.models.User], List[twitter.models.User]
+]:
+    # sanity check
+    utils.verify(api)
+    LOGGER.info(utils.get_rate_limit(api))
+
+    friends = utils.get_friends(api, start_user)
+
+    # < 500 tweets
+    less_than_500_tweets = sorted(
+        (user for user in friends if user.statuses_count < 500),
+        key=lambda u: u.statuses_count,
+        reverse=True
+    )
+
+    # haven't tweeted since 2018
+    inactive_accounts = sorted(
+        (
+            user for user in friends
+            if user.status and int(user.status.created_at.split()[-1]) < 2018
+        ),
+        key=lambda u: u.status.created_at_in_seconds,
+        reverse=True
+    )
+
+    LOGGER.info(utils.get_rate_limit(api))
+
+    return less_than_500_tweets, inactive_accounts
+
+
+def report(
+    less_than_500_tweets: List[twitter.models.User],
+    inactive_accounts: List[twitter.models.User],
+    filename: str
+) -> None:
+
+    with open(filename, 'w') as f:
+
+        f.write('less than 500 tweets\n')
+        f.write('=' * 20 + '\n')
+        for user in less_than_500_tweets:
+            f.write(
+                f"{user.statuses_count:<5} https://twitter.com/{user.screen_name} {user.name}\n"  # noqa
+            )
+        f.write('\n\n')
+
+        f.write('inactive users since 2018\n')
+        f.write('=' * 20 + '\n')
+        for user in inactive_accounts:
+            f.write(
+                f"{user.status.created_at}   https://twitter.com/{user.screen_name} {user.name}\n"  # noqa
+            )
+
+
+if __name__ == '__main__':
+    start_user = 'xatierlikelee'
+    api = utils.login()
+    less_than_500_tweets, inactive_accounts = go(api, start_user)
+    report(less_than_500_tweets, inactive_accounts, f'wtuf_{start_user}.txt')
